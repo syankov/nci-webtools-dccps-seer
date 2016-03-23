3410603ca69ce9e80044e187fab51f8e199525a5
scott75219 <scott.goldweber@gmail.com>
2016-03-23 12:24:20 -0400

    fixed queue to take dynamic input

--- jpsurv/jpsurv.py
+++ jpsurv/jpsurv.py
@@ -25,6 +25,20 @@ def fix_jpsurv(jpsurvDataString):
 def index():
     return render_template('index.html')
 
+def jsonp(func):
+    """Wraps JSONified output for JSONP requests."""
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            data = str(func(*args, **kwargs).data)
+            content = str(callback) + '(' + data + ')'
+            #mimetype = 'application/javascript'
+            mimetype = 'application/json'
+            return current_app.response_class(content, mimetype=mimetype)
+        else:
+            return func(*args, **kwargs)
+    return decorated_function
 
 @app.route('/jpsurvRest/parse', methods = ['GET'])
 def parse():
@@ -90,6 +104,9 @@ def stage1_upload():
     debug(OKGREEN+UNDERLINE+BOLD + "****** Stage 1: UPLOAD BUTTON ***** " + ENDC)
     tokenId = request.args.get('tokenId', False)
     info((BOLD + "****** Stage 1: tokenId = %s" + ENDC) % (tokenId))
+
+    #for k, v in request.args.iteritems():
+    #    print "var: %s = %s" % (k, v)
 
     file = request.files['file_control']
     if file and file.filename:
@@ -230,51 +247,50 @@ def stage3_recalculate():
     return current_app.response_class(out_json, mimetype=mimetype)
 
 
-@app.route('/jpsurvRest/stage4_trends_calculate', methods=['GET'])
-def stage4_trends_calculate():
-
-    print 'Go'
-
-    debug(OKGREEN+UNDERLINE+BOLD + "****** Stage 4: Trends BUTTON ***** " + ENDC)
-    info("Recalculating ...")
-
-    jpsurvDataString = request.args.get('jpsurvData', False)
-    jpsurvDataString = fix_jpsurv(jpsurvDataString)
+@app.route('/jpsurvRest/stage4_link', methods=['GET'])
+def stage4_link():
+    debug(OKGREEN+UNDERLINE+BOLD + "****** Stage 4: LINK BUTTON ***** " + ENDC)
     
+    #for k, v in request.args.iteritems():
+    #    print "var: %s = %s" % (k, v)
+    #name = request.get_json().get('jpsurvData', '')
+    #print name
+    jpsurvDataString = request.args.get('jpsurvData', True)
+    jpsurvDataString = fix_jpsurv(jpsurvDataString)
+
     info(BOLD+"**** jpsurvDataString ****"+ENDC)
     info(jpsurvDataString)
-    debug(OKBLUE+"The jpsurv STRING::::::"+ENDC)
-    debug(jpsurvDataString)
     jpsurvData = json.loads(jpsurvDataString)
-    #info(BOLD+"**** jpsurvData ****"+ENDC)
+
+    info(BOLD+"**** jpsurvData ****"+ENDC)
     for key, value in jpsurvData.iteritems():
         info("var: %s = %s" % (key, value))
-        print("var: %s = %s" % (key, value))
-    
+
     #Init the R Source
     rSource = robjects.r('source')
     rSource('./JPSurvWrapper.R')
 
-    info(BOLD+"**** Calling getTrendsData ****"+ENDC)
-    # Next two lines execute the R Program
-    getTrendsData = robjects.globalenv['getTrendsData']
-    getTrendsData(UPLOAD_DIR, jpsurvDataString)
+    info(BOLD+"**** Calling getDownloadOutputWrapper ****"+ENDC)
+    getDownloadOutputWrapper = robjects.globalenv['getDownloadOutputWrapper']
+    rStrVector = getDownloadOutputWrapper(UPLOAD_DIR, jpsurvDataString)
+
+    downloadLinkFileName = "".join(tuple(rStrVector))
+    info("Download File Name:  %s" % downloadLinkFileName)
+    link = "{\"link\":\"%s\"}" % (downloadLinkFileName)
+    #return json.dumps("{\"start.year\":[1975,2001],\"end.year\":[2001,2011],\"estimcate\":[-0.0167891169889347,-0.0032678676219079]}")
+    #print json.dumps(link)
     
-    status = '{"status":"OK"}'
     mimetype = 'application/json'
-    out_json = json.dumps(status)
-    print "Here it is"
-    print out_json
-
-    return current_app.response_class(out_json, mimetype=mimetype)
+    content = json.dumps(link)
+    return current_app.response_class(content, mimetype=mimetype)
 
 
-def sendqueue(jpsurvDataString):
+def sendqueue(jpsurvDataString,url,timetstamp,email,filepath):
     CONFIG = StompConfig('tcp://ncias-d1207-v.nci.nih.gov:61613')
     QUEUE = '/queue/jpsurv-dev'
     client = Stomp(CONFIG)
     client.connect()
-    client.send(QUEUE,json.dumps({'fileName':'test','path':'/home/user/python/testDir','email':'scott.goldweber@nih.gov','timeStamp':'2015-06-25','outputDir':'/local/content/analysistools/public_html/apps/jpsurv/tmp',"data":jpsurvDataString}))
+    client.send(QUEUE,json.dumps({'url':url,'email':email,'timeStamp':timetstamp,"filepath":filepath,"data":jpsurvDataString}))
     client.disconnect()
     
  
