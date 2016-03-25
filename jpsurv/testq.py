b88876b2751f392cf9b5db5f97b93dec0530fe9a
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2016-03-25 16:53:42 -0400

    Added a testq.py program for queue testing.

---
+++ jpsurv/testq.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import RequestProcessor
+
+def main():
+    """ Testing Queue for jpsurv """
+    print " Testing Queue "
+    print ""
+    print "Call sendRequest() here"
+
+if __name__ == '__main__':
+    UPLOAD_DIR = os.path.join(os.getcwd(), 'tmp')
+    QUEUE_DIR = os.path.join(os.getcwd(), 'queue')
+
+    sys.exit(main())
