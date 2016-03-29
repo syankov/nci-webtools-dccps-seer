590f8e6a1d7c2ef1ed36b972c171cf11a25b3dfe
scott75219 <scott.goldweber@gmail.com>
2016-03-29 12:50:15 -0400

    updated the queue

--- jpsurv/jpsurv.py
+++ jpsurv/jpsurv.py
@@ -8,6 +8,9 @@ from werkzeug import secure_filename
 import textwrap
 import logging
 import unicodedata
+from stompest.config import StompConfig
+from stompest.sync import Stomp
+from PropertyUtil import PropertyUtil
 
 app = Flask(__name__, static_folder='', static_url_path='/')
 
@@ -21,7 +24,6 @@ def fix_jpsurv(jpsurvDataString):
     print jpsurvDataString
 
     return jpsurvDataString
-
 def index():
     return render_template('index.html')
 
@@ -274,31 +276,29 @@ def queue():
 
     return current_app.response_class(out_json, mimetype=mimetype)
 
-@staticmethod
-def buildFailure(message):
-    response = jsonify(message=message, success=False)
-    response.mimetype = 'application/json'
-    response.status_code = 400
-    return response
+#def buildFailure(message):
+#    response = jsonify(message=message, success=False)
+#    response.mimetype = 'application/json'
+#    response.status_code = 400
+#    return response
     
-@staticmethod
-def buildSuccess(message):
-    response = jsonify(message=message, success=True)
-    response.mimetype = 'application/json'
-    response.status_code = 200
-    return response
+#def buildSuccess(message):
+#    response = jsonify(message=message, success=True)
+#    response.mimetype = 'application/json'
+#    response.status_code = 200
+#    return response
 
-def sendqueue(jpsurvDataString,url):
+def sendqueue(tokenId):
     try:
-        CONFIG = StompConfig('tcp://ncias-d1207-v.nci.nih.gov:61613')
-        client = Stomp(CONFIG)
+        QUEUE = jpsurvConfig.getAsString(QUEUE_NAME)
+        QUEUE_CONFIG=StompConfig(jpsurvConfig.getAsString(QUEUE_URL)) 
+        client = Stomp(QUEUE_CONFIG)
         client.connect()
-        client.send(QUEUE_DIR,json.dumps({'url':url,"filepath":QUEUE_DIR,"data":jpsurvDataString}))
+        client.send(QUEUE,json.dumps({"filepath":UPLOAD_DIR,"token":tokenId,"timestamp":"2015-06-25"}))
         client.disconnect()
-        return buildSuccess("The request has been received. An email will be sent when the calculation has completed.")
+#    return buildSuccess("The request has been received. An email will be sent when the calculation has completed.")
     except Exception as e:
-        return Pathway.buildFailure(str(e))
-    
+       return buildFailure(str(e))
  
 def info(msg):
     d={'clientip': request.remote_addr}
@@ -312,8 +312,10 @@ def debug(msg):
 
 import argparse
 if __name__ == '__main__':
+    QUEUE_NAME = 'queue.name'
+    QUEUE_URL = 'queue.url'
+    jpsurvConfig = PropertyUtil(r"config.ini")
     UPLOAD_DIR = os.path.join(os.getcwd(), 'tmp')
-    QUEUE_DIR = os.path.join(os.getcwd(), 'queue')
 
     #COLORS TO Make logging Mover visible
     HEADER = '\033[95m'
@@ -372,5 +374,6 @@ if __name__ == '__main__':
     logger.warning('app.debug: %s' % app.debug, extra=d)
     logger.warning('Log warning works.', extra=d)
     logger.info('Temp Directory: %s', BOLD+UPLOAD_DIR+ENDC, extra=d)
-
+    jpsurvDataString='{"file":{"dictionary":"Breast_RelativeSurvival.dic","data":"Breast_RelativeSurvival.txt","form":"form-506827.json"},"calculate":{"form":{"yearOfDiagnosisRange":[2000,2011],"cohortVars":["Age groups","Breast stage"],"cohortValues":["\"00-49\"","\"Localized\""],"covariateVars":"","maxjoinPoints":0},"static":{"yearOfDiagnosisTitle":"Year of diagnosis 1975+","years":["1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011"],"yearOfDiagnosisVarName":"Year_of_diagnosis_1975","seerFilePrefix":"Breast_RelativeSurvival","allVars":["Age groups","Breast stage","Year_of_diagnosis_1975"],"advanced":{"advDeleteInterval":"F","advBetween":"2","advFirst":"3","advLast":"4","advYear":"10"}}},"plot":{"form":{},"static":{"imageId":0}},"additional":{"headerJoinPoints":0,"yearOfDiagnosis":null,"intervals":[1,4]},"tokenId":"506827","status":"uploaded","stage2completed":0,"queue":{"email":"scott.goldweber@nih.com","url":"http://analysistools-sandbox.nci.nih.gov/jpsurv/?file_control_filename=Breast_RelativeSurvival.dic&file_data_filename=Breast_RelativeSurvival.txt&output_filename=form-506827.json&status=uploaded&tokenId=506827"}}'
+#    sendqueue(jpsurvDataString)
     app.run(host='0.0.0.0', port=port_num, debug = app.debug)
