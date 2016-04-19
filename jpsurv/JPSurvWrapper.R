0ba45a078df33ffe622e5b4f5e2c2f7248bfbcdd
scott75219 <scott.goldweber@gmail.com>
2016-04-19 12:55:47 -0400

    JPWrapper now grabs all jp's

--- jpsurv/JPSurvWrapper.R
+++ jpsurv/JPSurvWrapper.R
@@ -467,12 +467,24 @@ getTrendWrapper<- function (filePath,jpsurvDataString) {
   return(jsonl)
   
 }
-getJPWrapper<-function(filePath,jpsurvDataString)
+getJPWrapper<-function(filePath,jpsurvDataString,first_calc)
 {
   jpsurvData=fromJSON(jpsurvDataString)
   file=paste(filePath, paste("output-", jpsurvData$tokenId,".rds", sep=""), sep="/")
   outputData=readRDS(file)
-  JP=outputData$fittedResult$jp
+  
+  jpInd=jpsurvData$additional$headerJoinPoints
+  if(first_calc==TRUE)
+  {
+    jpInd=0
+    if(is.na(getSelectedModel(filePath,jpsurvDataString))==FALSE)
+    {
+      jpInd=getSelectedModel(filePath,jpsurvDataString)-1
+    }
+    
+  }
+  JP_List=outputData$fittedResult$FitList[[jpInd+1]]$apc[1]
+  JP=paste(JP_List[[1]],collapse=" ")
   return(JP)
 }
 
