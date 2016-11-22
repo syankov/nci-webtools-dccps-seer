31c47d3f21fb70143b17401f93fd08c2954f9171
Scott Goldweber <Scott.Goldweber@gmail.com>
2016-11-22 14:22:57 -0500

    testing

--- jpsurv/jpsurv.py
+++ jpsurv/jpsurv.py
@@ -29,7 +29,7 @@ OKGREEN = '\033[92m'
 WARNING = '\033[93m'
 FAIL = '\033[91m'
 BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
+UNDERLINE = '\033[4m' 
 ENDC = '\033[0m'
 
 def fix_jpsurv(jpsurvDataString):
